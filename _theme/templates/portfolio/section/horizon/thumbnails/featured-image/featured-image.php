258469cffbf6ca2c303532df7c0be50d86a96166
Joe McKie <joe@joemck.ie>
2013-07-11 23:41:22 +0100

    Put thumbnail inside anchor

--- _theme/templates/portfolio/section/horizon/thumbnails/featured-image/featured-image.php
+++ _theme/templates/portfolio/section/horizon/thumbnails/featured-image/featured-image.php
@@ -2,6 +2,6 @@
 <div class="featured-image-thumbnail">
 	<a class="thumb" href="<?php echo get_permalink();?>">
 		<span class="thumb-icon"></span>
+		<?php echo get_the_post_thumbnail( $post->ID, $image_size );?>
 	</a>
-	<?php echo get_the_post_thumbnail( $post->ID, $image_size );?>
 </div>
